2177869e37be3e62d7ee37ca81e8ada6392148e4
Paul-015 <polo.brunetonm@gmail.com>
2024-12-21 20:53:07 +0100

    Table sql Elements_Periodique

--- server/server.js
+++ server/server.js
@@ -1,39 +1,30 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-<<<<<<< HEAD
-=======
-// Import des middlewares
 const parseBody = require("./middlewares/parseBody");
 const checkAuth = require("./middlewares/checkAuth");
 const errorHandler = require("./middlewares/errorHandler");
 
-// Import des routes
->>>>>>> f04aadca12870ffbf970388d8f9753d56b206da3
 const authRoutes = require("./routes/authRoutes");
 const userRouter = require("./routes/user");
 const elementsRouter = require("./routes/elements"); // Changez cela pour correspondre au nom de votre route
 const securityRouter = require("./routes/security");
 
-<<<<<<< HEAD
-=======
+
 // Charger les variables d'environnement depuis .env
->>>>>>> f04aadca12870ffbf970388d8f9753d56b206da3
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
-<<<<<<< HEAD
+
 app.use(express.json()); 
-=======
 // Middleware pour analyser les données JSON
 app.use(express.json()); 
 
 // Middlewares personnalisés
 app.use(parseBody);  // Middleware de parsing du corps de la requête
 app.use(checkAuth);  // Middleware d'authentification
->>>>>>> f04aadca12870ffbf970388d8f9753d56b206da3
 
 app.get("/", (req, res) => {
   res.send("Hello world !!");
@@ -44,8 +35,7 @@ app.use("/api/users", userRouter);
 app.use("/api/elements", elementsRouter); 
 app.use("/api/security", securityRouter); 
 
-<<<<<<< HEAD
-=======
+
 // Middleware pour les routes non trouvées (404)
 app.use((req, res, next) => {
   const error = new Error("Route introuvable");
@@ -69,7 +59,7 @@ process.on("unhandledRejection", (reason, promise) => {
 });
 
 // Démarrage du serveur
->>>>>>> f04aadca12870ffbf970388d8f9753d56b206da3
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
